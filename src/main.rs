9d3d5e2ec9ea13b45d8d4b3593e3f549e30b0599
Peter Romančík <xromanc1@fi.muni.cz>
2024-08-17 15:58:46 +0200

    feat: Add clearly overengineered discord bot

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,248 @@
-use std::fs::{read_to_string, write};
-use std::process::exit;
+use std::{
+    collections::HashSet,
+    env, process,
+    sync::Arc,
+    time::{Duration, SystemTime},
+};
 
-use kaktus::scrape::{get_kaktus_page, get_latest_message};
+use serenity::{
+    async_trait,
+    builder::{CreateEmbed, CreateMessage},
+    model::{channel::Message, gateway::Ready, id::ChannelId},
+    prelude::*,
+};
 
-const FILE_PATH: &str = "last_message";
+use kaktus::scrape::{get_kaktus_page, get_latest_message, KaktusMessage};
 
-// TODO use cron to run the script every few hours
-// Maybe use tmpfs to make the file stay in ram,
-// therefore making the SD card last longer
-//
-// maybe loop and sleep for hour might be better for the card
+struct LastMessage;
 
-fn main() {
-    let Ok(old_message) = read_to_string(FILE_PATH) else {
-        // TODO the bot itself can send this
-        eprintln!("Unable to read the file containg the old message");
-        exit(1)
+impl TypeMapKey for LastMessage {
+    type Value = Arc<RwLock<KaktusMessage>>;
+}
+
+struct LastUpdate;
+
+impl TypeMapKey for LastUpdate {
+    type Value = Arc<RwLock<SystemTime>>;
+}
+
+struct RegisteredChannels;
+
+impl TypeMapKey for RegisteredChannels {
+    type Value = Arc<RwLock<HashSet<ChannelId>>>;
+}
+
+struct Handler;
+
+const SLEEP_TIME: u64 = 3600;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        match msg.content.as_ref() {
+            "!status" => {
+                let last_message = {
+                    let data_read = ctx.data.read().await;
+                    data_read
+                        .get::<LastMessage>()
+                        .expect("LastMessage must be in the type map")
+                        .clone()
+                        .read()
+                        .await
+                        .clone()
+                };
+
+                let last_update_time = {
+                    let data_read = ctx.data.read().await;
+                    *data_read
+                        .get::<LastUpdate>()
+                        .expect("LastUpdate must be in the type map")
+                        .clone()
+                        .read()
+                        .await
+                };
+
+                let time_till_next_update = Duration::from_secs(SLEEP_TIME)
+                    .saturating_sub(SystemTime::now().duration_since(last_update_time).unwrap());
+
+                let message = CreateMessage::new().embed(
+                    CreateEmbed::new()
+                        .title("Status update")
+                        .field(last_message.header, last_message.content, false)
+                        .field(
+                            "next update",
+                            format!("In {time_till_next_update:?}"),
+                            false,
+                        ),
+                );
+
+                if let Err(why) = msg.channel_id.send_message(&ctx.http, message).await {
+                    eprintln!("Error sending message: {why:?}");
+                    return;
+                }
+            }
+            "!register" => {
+                let register_lock = {
+                    let data_read = ctx.data.read().await;
+                    data_read
+                        .get::<RegisteredChannels>()
+                        .expect("RegisteredChannels must be in the type map")
+                        .clone()
+                };
+
+                {
+                    let mut registered_channels = register_lock.write().await;
+
+                    registered_channels.insert(msg.channel_id);
+                    println!("{:?}", registered_channels);
+                    println!("{:?}", registered_channels);
+                    if let Err(why) = msg
+                        .channel_id
+                        .say(&ctx.http, "This channel will get updates from now on!")
+                        .await
+                    {
+                        eprintln!("Error sending message: {why:?}");
+                        return;
+                    }
+                };
+            }
+            "!unregister" => {
+                let register_lock = {
+                    let data_read = ctx.data.read().await;
+                    data_read
+                        .get::<RegisteredChannels>()
+                        .expect("RegisteredChannels must be in the type map")
+                        .clone()
+                };
+
+                {
+                    let mut registered_channels = register_lock.write().await;
+
+                    registered_channels.remove(&msg.channel_id);
+                    if let Err(why) = msg
+                        .channel_id
+                        .say(
+                            &ctx.http,
+                            "This channel will no longer get updates from now on!",
+                        )
+                        .await
+                    {
+                        eprintln!("Error sending message: {why:?}");
+                        return;
+                    }
+                }
+            }
+            _ => {}
+        }
+        if msg.content == "!register" {}
+    }
+
+    async fn ready(&self, ctx: Context, _ready: Ready) {
+        let ctx = Arc::new(ctx);
+        let ctx_copy = ctx.clone();
+
+        tokio::spawn(async move {
+            loop {
+                let Ok(page) = get_kaktus_page().await else {
+                    eprintln!("Unable to download the page");
+                    return;
+                };
+                let Some(new_message) = get_latest_message(&page) else {
+                    eprintln!("Unable to fetch the latest message");
+                    return;
+                };
+
+                {
+                    let data_read = ctx_copy.data.read().await;
+                    let lock = data_read
+                        .get::<LastUpdate>()
+                        .expect("LastUpdate must be in the type map")
+                        .clone();
+                    let mut last_update = lock.write().await;
+                    *last_update = SystemTime::now()
+                }
+
+                let mut changed = false;
+                {
+                    let data_read = ctx_copy.data.read().await;
+                    let lock = data_read
+                        .get::<LastMessage>()
+                        .expect("LastUpdate must be in the type map")
+                        .clone();
+                    let mut last_message = lock.write().await;
+
+                    if *last_message != new_message {
+                        *last_message = new_message.clone();
+                        changed = true;
+                    }
+                }
+
+                let register_lock = {
+                    let data_read = ctx.data.read().await;
+                    data_read
+                        .get::<RegisteredChannels>()
+                        .expect("RegisteredChannels must be in the type map")
+                        .clone()
+                };
+
+                let message =
+                    CreateMessage::new().embed(CreateEmbed::new().title("New message!").field(
+                        new_message.header,
+                        new_message.content,
+                        true,
+                    ));
+
+                {
+                    let registered_channels = register_lock.read().await;
+
+                    if changed {
+                        for channel_id in registered_channels.iter() {
+                            if let Err(why) = channel_id
+                                .send_message(&ctx_copy.http, message.clone())
+                                .await
+                            {
+                                eprintln!("Error sending message: {why:?}");
+                            };
+                        }
+                    }
+                }
+
+                tokio::time::sleep(Duration::from_secs(SLEEP_TIME)).await;
+            }
+        });
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("The DISCORD_TOKEN needs to be set in ENV");
+    let default_channel_id: ChannelId = env::var("DISCORD_CHANNEL_ID")
+        .expect("The DISCORD_CHANNEL_ID needs to be set in ENV")
+        .parse()
+        .expect("Incorrent DISCORD_CHANNEL_ID");
+
+    let intents = GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let Ok(mut client) = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+    else {
+        process::exit(1);
     };
 
-    let Ok(page) = get_kaktus_page() else {
-        // TODO the bot itself can send this
-        eprintln!("Unable to download the page");
-        exit(1)
-    };
-    let Some(new_message) = get_latest_message(&page) else {
-        // TODO the bot itself can send this
-        eprintln!("Unable to fetch the latest message");
-        exit(1)
-    };
+    {
+        let mut data = client.data.write().await;
+        data.insert::<LastMessage>(Arc::new(RwLock::new(KaktusMessage::default())));
+        data.insert::<RegisteredChannels>(Arc::new(RwLock::new(HashSet::from([
+            default_channel_id,
+        ]))));
+        data.insert::<LastUpdate>(Arc::new(RwLock::new(SystemTime::now())));
+    }
 
-    if old_message != new_message {
-        println!("We got a new message!");
-        if write(FILE_PATH, new_message).is_err() {
-            // TODO the bot itself can send this
-            eprintln!("Unable to write the new message to the file");
-            exit(1)
-        };
+    if let Err(why) = client.start().await {
+        eprintln!("Client error: {why:?}");
+        process::exit(1);
     }
 }
--- src/scrape.rs
+++ src/scrape.rs
@@ -1,8 +1,14 @@
-use reqwest::{blocking::ClientBuilder, cookie::Jar, Url};
+use reqwest::{cookie::Jar, ClientBuilder, Url};
 use scraper::{Html, Selector};
 use std::sync::Arc;
 
-pub fn get_kaktus_page() -> Result<String, reqwest::Error> {
+#[derive(Clone, PartialEq, Default)]
+pub struct KaktusMessage {
+    pub header: String,
+    pub content: String,
+}
+
+pub async fn get_kaktus_page() -> Result<String, reqwest::Error> {
     // the page does not load without this cookie
     let cookie = "COOKIE_SUPPORT=true;";
     let url = "https://www.mujkaktus.cz/homepage".parse::<Url>().unwrap();
@@ -15,12 +21,12 @@ pub fn get_kaktus_page() -> Result<String, reqwest::Error> {
         .build()?;
 
     let request = client.get(url).build()?;
-    let response = client.execute(request)?;
+    let response = client.execute(request).await?;
 
-    response.text()
+    response.text().await
 }
 
-pub fn get_latest_message(page: &str) -> Option<String> {
+pub fn get_latest_message(page: &str) -> Option<KaktusMessage> {
     let document = Html::parse_document(page);
 
     // unwraps are safe, since the selectors are valid
@@ -33,5 +39,8 @@ pub fn get_latest_message(page: &str) -> Option<String> {
     let latest_header = latest_message.select(&h3_selector).next()?.text().next()?;
     let latest_text = latest_message.select(&p_selector).next()?.text().next()?;
 
-    Some(format!("{latest_header};{latest_text}"))
+    Some(KaktusMessage {
+        header: latest_header.to_string(),
+        content: latest_text.to_string(),
+    })
 }
